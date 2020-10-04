613f0b3367c3b861f9a9e123832f31aca2f3792e
Joseph Eagar <joeedh@gmail.com>
2020-10-03 19:11:19 -0700

    Forgot this file

---
+++ scripts/solver.js
@@ -0,0 +1,15 @@
+let _solver = undefined;
+
+define([
+    "./util", "./vectormath", "./const"
+], function(util, vectormath, cconst) {
+  "use strict";
+  
+  let exports = _solver = {};
+  
+  let gridoffs = cconst.gridoffs;
+  
+  
+  return exports;
+});
+
